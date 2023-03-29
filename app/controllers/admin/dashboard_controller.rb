b38a6bb526ecccb47919d48431212087e5db0216
gilcier <gilcier.sousa@alfaconcursos.com.br>
2023-03-29 12:45:47 -0300

    improve dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < AdminController
   def index
+    @count_companies = Company.count
+    @count_employees = Employee.count
+    @count_users = User.count
   end
 end
